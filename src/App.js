9d84b5b18ebda48971118fc284fccaa2c45051d5
Laura Kenny <laura.kenny@oetkerdigital.com>
2021-10-27 09:11:26 +0200

    Format question rows and pass props to card

--- src/App.js
+++ src/App.js
@@ -6,27 +6,31 @@ import { useEffect, useState } from 'react';
 export default function App() {
   const [questions, setQuestions] = useState([]);
 
-  const getQuestions = () => {
-    const requestOptions = {
-      method: 'GET',
-      headers: {
-        Accept: 'application/json',
-        'Content-Type': 'application/json',
-      },
-    };
-    fetch('https://polls.apiblueprint.org/questions', requestOptions)
-      .then(function (response) {
-        if (!response.ok) {
-          throw new Error(`HTTP error! status: ${response.status}`);
-        }
-        return response.json();
-      })
-      .then(function (data) {
-        setQuestions(data);
-      });
-  };
+  function formatQuestionRows(questionData) {
+    const rows = [...Array(Math.ceil(questionData.length / 4))];
+    return rows.map((row, i) => questionData.slice(i * 4, i * 4 + 4));
+  }
 
   useEffect(() => {
+    const getQuestions = () => {
+      const requestOptions = {
+        method: 'GET',
+        headers: {
+          Accept: 'application/json',
+          'Content-Type': 'application/json',
+        },
+      };
+      fetch('https://polls.apiblueprint.org/questions', requestOptions)
+        .then(function (response) {
+          if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+          }
+          return response.json();
+        })
+        .then(function (data) {
+          setQuestions(formatQuestionRows(data));
+        });
+    };
     getQuestions();
   }, []);
 
@@ -38,23 +42,19 @@ export default function App() {
       <div className="columns is-desktop">
         <div className="column"></div>
         <div className="column is-four-fifths">
-          <div className="columns">
-            <div className="column">
-              <Card />
-            </div>
-            <div className="column">
-              <Card />
-            </div>
-            <div className="column">
-              <Card />
-            </div>
-            <div className="column">
-              <Card />
-            </div>
-            <div className="column">
-              <Card />
-            </div>
-          </div>
+          {questions.map((row, i) => {
+            return (
+              <div className="columns" key={i}>
+                {row.map((question, i) => {
+                  return (
+                    <div className="column is-one-quarter" key={i}>
+                      <Card question={question} />
+                    </div>
+                  );
+                })}
+              </div>
+            );
+          })}
         </div>
         <div className="column"></div>
       </div>
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,11 +1,11 @@
 export default function Card({ question }) {
   return (
-    <div className="card">
+    <div className="card has-background-success-light">
       <div className="card-content">
         <div>
-          <p className="title is-4">What is your favourite animal?</p>
-          <time dateTime="2016-1-1">11:09 PM - 1 Jan 2016</time>
-          <p>Choices: 3</p>
+          <p className="title is-5">{question.question}</p>
+          <time dateTime="2016-1-1">{question.published_at}</time>
+          <p>Choices: {question.choices.length}</p>
         </div>
       </div>
     </div>
